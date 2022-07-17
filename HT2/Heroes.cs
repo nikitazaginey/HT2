2656421864a9d38119abebc5c79a1dcfd2608633
nikitazaginey <nikitazaginey1@gmail.com>
2022-07-17 16:30:07 +0200

    Add project files.

---
+++ HT2/Heroes.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace HT2
+{
+    public abstract class Heroes
+    {
+        // Define the Warriors properties
+        public string Name { get; set; } 
+        public int Health { get; set; } 
+        public int AttkMax { get; set; }
+        public int AttkMin { get; set; }
+        public int BlockMax { get; set; }
+        public Heroes(string name, int health, int attkMax, int attkMin, int blockMax)
+        {
+            Name = name;
+            Health = health;
+            AttkMax = attkMax;
+            AttkMin = attkMin;
+            BlockMax = blockMax;
+        }
+        public double Attack() => new Random().Next(AttkMin, AttkMax);
+        public  double Block () =>  new Random().Next(1, BlockMax);
+        public abstract void SpecialAbilities();
+
+    }
+}
