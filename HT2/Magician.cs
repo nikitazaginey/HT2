7a2e49ba36836a9c3d6cc3a11ab96fde2ed3c66c
nikitazaginey <nikitazaginey1@gmail.com>
2022-07-18 11:38:10 +0200

    add class Magician and fix class Swordsman

---
+++ HT2/Magician.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace HT2
+{
+    public class Magician : Heroes
+    {
+        public Magician(string name, int health, int attkMax, int attkMin, int blockMax)
+           : base("Magician", 70, 40, 25, 10) { }
+        public override void SpecialAbilities()
+        {
+            Console.WriteLine($"Unit {Name} use special magic!");
+            AttkMax = 50;
+            AttkMin = 35;
+        }
+    }
+}
--- HT2/Swordsmen.cs
+++ HT2/Swordsmen.cs
@@ -6,7 +6,7 @@ namespace HT2
     public  class Swordsman : Heroes
     {
         public Swordsman(string name, int health, int attkMax, int attkMin, int blockMax)
-            : base("swordsman", 100, 30, 20, 10) { }
+            : base("Swordsman", 100, 30, 20, 10) { }
         public override void SpecialAbilities()
         {
             Console.WriteLine($"Unit {Name} has not special abilities");
