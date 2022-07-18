4061c8a41ca618d00b608d43a496728f48e96617
nikitazaginey <nikitazaginey1@gmail.com>
2022-07-18 17:27:18 +0200

    add main character class

---
+++ HT2/Heroes/MainСharacter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HT2
+{
+    internal class MainСharacter : Heroes
+    {
+        public MainСharacter(string name)
+          : base(name, 500, 60, 40, 25) { }
+        public override void SpecialAbilities()
+        {
+            Console.WriteLine($"You drink special elixir! Your defense is improved");
+            BlockMax = 70;
+        }
+    }
+}
