13c8aea47bebab792e3c683b34f6277725680f1d
nikitazaginey <nikitazaginey1@gmail.com>
2022-07-18 11:28:00 +0200

    add class Swordsman

---
+++ HT2/Swordsmen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace HT2
+{
+    public  class Swordsman : Heroes
+    {
+        public Swordsman(string name, int health, int attkMax, int attkMin, int blockMax)
+            : base("swordsman", 100, 30, 20, 10) { }
+        public override void SpecialAbilities()
+        {
+            Console.WriteLine($"Unit {Name} has not special abilities");
+        }
+    }
+}
