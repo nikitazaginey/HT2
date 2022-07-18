e882f811a38d981ca8f70fb3b0fd62662ef9b7ae
nikitazaginey <nikitazaginey1@gmail.com>
2022-07-18 11:57:36 +0200

    add class elf and add folder for heroes

---
+++ HT2/Heroes/Elf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace HT2
+{
+    public class Elf : Heroes
+    {
+        public Elf(string name, int health, int attkMax, int attkMin, int blockMax)
+          : base("Elf", 85, 35, 25, 20) { }
+        public override void SpecialAbilities()
+        {
+            Console.WriteLine($"Unit {Name} use special protection!");
+            BlockMax = 30;
+        }
+    }
+}
